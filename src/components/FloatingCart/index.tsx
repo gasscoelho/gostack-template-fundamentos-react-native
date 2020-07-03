dac8d2810c43703f923e1c51bb4fb14e26dd85cd
BlaackRabbit <33602013+Potato@users.noreply.github.com>
2020-07-02 23:16:13 -0300

    feat: implement FloatingCart component

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,19 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const total = products.reduce((accumulator, prod) => {
+      return accumulator + prod.price * prod.quantity;
+    }, 0);
 
-    return formatValue(0);
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, prod) => {
+      return accumulator + prod.quantity;
+    }, 0);
 
-    return 0;
+    return total;
   }, [products]);
 
   return (
